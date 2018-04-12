aad0bde75fb6b15d386d1a76e58453bf4f351c66
Crazyhead90 <20604229+Crazyhead90@users.noreply.github.com>
2018-04-12 17:24:52 +0200

    Update install.go

--- cmd/pi64-build/install.go
+++ cmd/pi64-build/install.go
@@ -81,7 +81,7 @@ deb-src http://deb.debian.org/debian stretch main contrib non-free
 deb http://deb.debian.org/debian stretch-updates main contrib non-free
 deb-src http://deb.debian.org/debian stretch-updates main contrib non-free
 
-deb http://security.debian.org/debian-security/ stretch/updates maincontrib non-free
+deb http://security.debian.org/debian-security/ stretch/updates main contrib non-free
 deb-src http://security.debian.org/debian-security/ stretch/updates main contrib non-free
 `)
 
